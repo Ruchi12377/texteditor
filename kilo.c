6140523b59ce8596cf42bc85781cbee012106030
Ruchi <rappinngu2006@gmail.com>
2024-10-13 18:47:27 +0900

    chapter 02

--- kilo.c
+++ kilo.c
@@ -1,3 +1,68 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
+
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) -1) die("tcgetattr");
+    // プログラムの終了時に関数を呼び出す
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    // ローカルフラグをオフにする
+    // ICRNL Ctrl-Mを無効にする (carriage retrun new line)
+    // IXION Ctrl-SとCtrl-Qを無効にする
+    raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    // OPOST 出力後の後処理(\nから\r\nへの変換)をオフにする
+    raw.c_lflag &= ~(OPOST);
+    raw.c_lflag |= (CS8);
+    // ECHO 表示をオフにする
+    // ICANON カノニカルモードをオフにする
+    // IEXTEN Ctrl-Vを無効にする、MacでCtrl-Oを無効にする
+    // ISIG Ctrl-Cなどを無効にする
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*** init ***/
+
 int main() {
+    enableRawMode();
+
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read"); 
+        // 文字が制御文字かどうかをテストする
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            // 97 ('a')
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
     return 0;
 }
